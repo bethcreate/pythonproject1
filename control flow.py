06c1839a6d724a6e4d0f5a42fc6f77e349a773c6
bethcreate <kamaubetth@gmail.com>
2021-04-27 14:11:50 +0300

    first commit

---
+++ control flow.py
@@ -0,0 +1,42 @@
+n = range(10)
+for x in n:
+    if x%2==0:
+        print("{} is divisible by 2".format(x))
+    elif x%3==0:
+        print("{} is divisible by 3".format(x))
+    else:
+        print("{} is not divisible by 2 or 3".format(x))
+
+x=1
+while x <10:
+    print(x)
+    x +=1
+
+
+
+    x= 100
+    while x >= 30:
+        print(x)
+        x-=10
+
+
+
+    z = 1
+    while z < 50:
+        print(z)
+        if z/7:
+            break
+            z -= 10
+
+
+a= 0
+while x < 10:
+    x += 1
+    if x%2 ==0:
+        continue
+        print(x)
+
+
+
+
+
---
+++ madlibz.py
@@ -0,0 +1,18 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural_noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love" + celebrity)
+
+name = input("Enter a name: ")
+hobbies = input("Enter a hobby: ")
+country = input("Enter a country: ")
+
+
+print(name + " loves")
+print(hobbies + " when not working")
+print(" and she lives in" + country)
+
+
